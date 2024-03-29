560d33059e594ef812ca3b6cdaf60a60e5eb64a7
rafacoper <rafaelbarbosa.tecnologia@outlook.com>
2024-03-28 22:20:10 -0300

    Retirei foto do sidebar. Vai ficar sem por enquanto

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,27 +1,34 @@
 import { useState } from "react";
-import { Sidebar, Menu } from "react-pro-sidebar";
-import { Box, useTheme } from "@mui/material";
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
-import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
-import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
-import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
-import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
-import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
-import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
-import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
-import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
-import CollapsedMenu from "../../components/CollapsedMenu";
-import ExpandedMenu from "../../components/ExpandedMenu";
+import { Sidebar, Menu, sidebarClasses, menuClasses, MenuItem } from "react-pro-sidebar";
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutline";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 import SideBarItem from "../../components/SideBarItem";
 import { tokens } from "../../theme";
 
 const menuItems = [
   { title: "Dashboard", to: "/", icon: <HomeOutlinedIcon /> },
   { title: "Manage Team", to: "/team", icon: <PeopleOutlinedIcon /> },
-  { title: "Contacts Information", to: "/contacts", icon: <ContactsOutlinedIcon /> },
-  { title: "Invoices Balances", to: "/invoices", icon: <ReceiptOutlinedIcon /> },
+  {
+    title: "Contacts Information",
+    to: "/contacts",
+    icon: <ContactsOutlinedIcon />,
+  },
+  {
+    title: "Invoices Balances",
+    to: "/invoices",
+    icon: <ReceiptOutlinedIcon />,
+  },
   { title: "Profile Form", to: "/form", icon: <PersonOutlinedIcon /> },
   { title: "Calendar", to: "/calendar", icon: <CalendarTodayOutlinedIcon /> },
   { title: "FAQ Page", to: "/faq", icon: <HelpOutlineOutlinedIcon /> },
@@ -38,41 +45,77 @@ const MySidebar = () => {
   const [selected, setSelected] = useState("Dashboard");
 
   return (
-      <Sidebar 
-        collapsed={isCollapsed}
-        style={{
-          backgroundColor: colors.primary[400],
-          "& .pro-icon-wrapper": {
-            backgroundColor: "transparent !important",
-          },
-          "& .pro-inner-item": {
-            padding: "5px 35px 5px 20px !important",
-          },
-          "& .pro-inner-item:hover": {
-            color: "#868dfb !important",
-          },
-          "& .pro-menu-item.active": {
-            color: "#6870fa !important",
-          },
-        }}
-      >
-        <Menu iconShape="square">
-          <CollapsedMenu setIsCollapsed={setIsCollapsed} />
-          {!isCollapsed && <ExpandedMenu />}
-          <Box paddingLeft={isCollapsed ? undefined : "10%"}>
-            {menuItems.map((item, index) => (
-              <SideBarItem
-                key={index}
-                title={item.title}
-                to={item.to}
-                icon={item.icon}
-                selected={selected}
-                setSelected={setSelected}
-              />
-            ))}
-          </Box>
-        </Menu>
-      </Sidebar>
+    <Sidebar
+      collapsed={isCollapsed}
+      rootStyles={{
+        [`.${sidebarClasses.container}`]: {
+          backgroundColor: `${colors.primary[400]}`,
+        },
+        [`.${menuClasses.active}`]: {
+          color: "#6870fa !important",
+        },
+        [`.${menuClasses.button}:hover`]: {
+          color: "#868dfb !important",
+          backgroundColor: "transparent !important",
+        },
+      }}
+    >
+      <Menu iconShape="square">
+        <MenuItem
+          onClick={() => setIsCollapsed(!isCollapsed)}
+          icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
+          style={{
+            margin: "10px 0 20px 0",
+            color: colors.grey[100],
+          }}
+        >
+          {!isCollapsed && (
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+              ml="15px"
+            >
+              <Typography variant="h3" color={colors.grey[100]}>
+                ADMIN
+              </Typography>
+              <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
+                <MenuOutlinedIcon />
+              </IconButton>
+            </Box>
+            )}
+        </MenuItem>
+          {!isCollapsed && (
+            <Box mb="25px">
+              <Box textAlign="center">
+                <Typography
+                  variant="h2"
+                  color={colors.grey[100]}
+                  fontWeight="bold"
+                  sx={{ m: "10px 0 0 0" }}
+                >
+                 Rafael Coper
+                </Typography>
+                <Typography variant="h5" color={colors.greenAccent[500]}>
+                  Im the supervisor
+                </Typography>
+              </Box>
+            </Box>
+          )}
+        <Box paddingLeft={isCollapsed ? undefined : "10%"}>
+          {menuItems.map((item, index) => (
+            <SideBarItem
+              key={index}
+              title={item.title}
+              to={item.to}
+              icon={item.icon}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          ))}
+        </Box>
+      </Menu>
+    </Sidebar>
   );
 };
 
