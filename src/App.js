0908413478cccb158a5a9db1d1e82ce705d351ca
rafacoper <rafaelbarbosa.tecnologia@outlook.com>
2024-04-05 12:37:15 -0300

    Calendar ok

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
+import Calendar from "./scenes/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -28,6 +29,7 @@ function App() {
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
             </Routes>
           </main>
         </div>
